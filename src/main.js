70a1f355088939da5087754b1805e01620fc2b6e
Endre Solem <esolem@gmail.com>
2018-02-04 10:27:43 +0100

    deletes some starter files and starting all over

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './components/app';
 
-new Vue({
+
+new Vue ({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
 })
