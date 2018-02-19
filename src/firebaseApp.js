cffcb9b36d971f643c4436cfd47c7f086fc3b4d6
endre solem <esolem@gmail.com>
2018-02-19 22:33:32 +0100

    adding firebase connection

---
+++ src/firebaseApp.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBznrw-OE3aMCXXS9fCYYJw3Kb8MoBsJic",
+    authDomain: "agilitevue.firebaseapp.com",
+    databaseURL: "https://agilitevue.firebaseio.com",
+    projectId: "agilitevue",
+    storageBucket: "",
+    messagingSenderId: "360181244574"
+  };
+
+  export const firebaseApp = firebase.initializeApp(config);
