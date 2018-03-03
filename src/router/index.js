8dba171dd7cff473f57bc7fb29ba7a82d99dff44
endre solem <esolem@gmail.com>
2018-03-03 16:13:22 +0100

    reorder functions in init

--- src/router/index.js
+++ src/router/index.js
@@ -9,13 +9,6 @@ import Profile from '../components/profile';
 
 Vue.use(VueRouter);
 
-const routes = [
-    {path: '/', name: 'Competitions', component: Competitions },
-    {path: '/signin', name: 'SignIn', component: SignIn },
-    {path: '/signup', name: 'SignUp', component: SignUp },
-    {path: '/profile', name: 'Profile', component: Profile, beforeEnter: ifAuthenticated },
-]
-
 const ifAuthenticated = (to, from, next) => {
   console.log('isauth:', store.getters);
   if (store.getters.isAuthenticated) {
@@ -24,6 +17,13 @@ const ifAuthenticated = (to, from, next) => {
   }
   next('/signin');
 }
+
+const routes = [
+    {path: '/', name: 'Competitions', component: Competitions },
+    {path: '/signin', name: 'SignIn', component: SignIn },
+    {path: '/signup', name: 'SignUp', component: SignUp },
+    {path: '/profile', name: 'Profile', component: Profile, beforeEnter: ifAuthenticated },
+]
 
 const router = new VueRouter({
     mode: 'history',
