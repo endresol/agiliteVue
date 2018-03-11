6b9bcfc782bfea7b1bb4c9b36e268a4d91c810c5
Endre Solem <esolem@gmail.com>
2018-03-11 23:58:50 +0100

    form for competitions and started work on club-registration"

--- src/components/competitions/AddCompetition.vue
+++ src/components/competitions/AddCompetition.vue
@@ -1,15 +1,66 @@
 <template>
-  
+  <div class="competition">
+    <div class="form">
+      <div class="field">
+        <label for="label-name" class="label">Navn</label>
+        <div class="control">
+          <input type="text" class="input" id="label-name" v-model="competition.name">
+        </div>
+      </div>
+      <div class="field is-grouped">
+        <div class="control">
+          <label for="label-date" class="label">Dato</label>
+          <input type="date" class="input" id="label-date" v-model="competition.startdate">
+        </div>
+        <div class="control">
+          <label for="label-time" class="label">Klokkeslett</label>
+          <input type="time" class="input" id="label-time" v-model="competition.starttime">
+        </div>
+      </div>
+      <div class="field">
+        <label for="label-level" class="label">Klasser</label>
+        <div class="control">
+          <input type="text" class="input" id="label-level" v-model="competition.levels">
+        </div>
+      </div>
+      <div class="field">
+        <label for="label-sizes" class="label">Størrelser</label>
+        <div class="control">
+          <input type="text" class="input" id="label-sizes" v-model="competition.sizes">
+        </div>
+      </div>
+      <div class="field">
+        <label for="label-price" class="label">Pris</label>
+        <div class="control">
+          <input type="text" class="input" id="label-price" v-model="competition.price">
+        </div>
+      </div>
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link" @click="addDog">Submit</button>
+        </div>
+        <div class="control">
+          <button class="button is-text" @click="closeform">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return (
+    return {
       competition: {
-        name: '',
+        name: null,
+        startdate: null,
+        starttime: null,
+        levels: [],
+        sizes: [],
+        price: null,
+        organizer: '',
       },
-    )
+    }
   }
 }
 </script>
--- src/components/competitions/index.vue
+++ src/components/competitions/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    <h1 class="title">App component</h1>
-    <a class="button is-primary">Hei</a>
-    {{$store.state.competitions}}
+    <div class="content">
+      <h1 class="title">Kommende konkurranser</h1>
+      <div class="columns">
+        <div class="column">
+          {{$store.state.competitions}}
+        </div>
+        <div class="column">
+          <AddCompetition />
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 import { db } from '../../firebaseApp';
+import AddCompetition from './AddCompetition';
+
 export default {
+  components: {
+    AddCompetition,
+  },
   mounted() {
     db.collection('competitions').get().then('value', snap => {
       let competitions = [];
